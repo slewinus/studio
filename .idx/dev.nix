ca155d342f6a997a6423b787c3541c03b782889e
Oscar <oscar.robertbesle@gmail.com>
2025-04-11 09:21:17 +0000

    i want to change the name of the db to db-postgress everywhere so i can do the migration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,17 +3,24 @@
 {pkgs}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.sudo
   ];
+
   # Sets environment variables in the workspace
   env = {};
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
+
     workspace = {
       onCreate = {
         default.openFiles = [
@@ -21,6 +28,7 @@
         ];
       };
     };
+
     # Enable previews and customize configuration
     previews = {
       enable = true;
